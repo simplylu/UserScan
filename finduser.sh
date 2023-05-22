0d8d2e456327c1dbc62947d3220db034d84e3f54
js-on <jakobs@js-on.de>
2023-05-22 14:29:49 +0200

    Added Twitch

--- finduser.sh
+++ finduser.sh
@@ -1032,6 +1032,20 @@ scanner() {
         printf "https://%s.basecamphq.com/login\n" $username >> $username.txt
         
     fi
+
+    ## Twitch
+
+    printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Twitch: \e[0m"
+    check1=$(curl -s -i "https://www.twitch.tv/$username" | grep "<meta property='twitter:site' content='@twitch'>" ; echo $?)
+
+    if [[ $check1 == *'0'* ]]; then
+        printf "\e[1;93mNot Found!\e[0m\n"
+    elif [[ $check1 == *'1'* ]]; then
+        printf "\e[1;92m Found!\e[0m https://www.twitch.tv/%s\n" $username
+        printf "https://www.twitch.tv/$username\n" $username >> $username.txt
+    fi
+
+
     partial
 }
 
